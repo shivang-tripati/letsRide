1e6436d555dca7887e27ccd27439570705068af6
Shivang Tripathi <shivangtripathi101@gmail.com>
2023-10-21 19:08:19 +0530

    fix credential issue

--- src/screens/RequestScreen.js
+++ src/screens/RequestScreen.js
@@ -1,13 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, FlatList, Dimensions } from 'react-native';
 import React from 'react'
 import tw from 'twrnc';
 import MapComponent from '../components/MapComponent';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Avatar, Icon } from 'react-native-elements';
 
 const RequestScreen = () => {
   return (
-    <View style={tw``}>
+   
+    <View style={tw`mt-8 ml-7 mr-7 flex-1`}>
+      <View style={tw`flex-row`}>
+      <View style={tw` w-8 ml-5 `}>
+        <Icon
+            name='arrow-back'
+            type='materialicons'
+            size={26}
+            color='black'
+
+        />
+
+      </View>
+      <View style={tw`ml-15 flex-row`}>
+        <TouchableOpacity>
+          <View style={tw`flex-row`}>
+            <Avatar
+                 rounded
+                 avatarStyle={{}}
+                 size={30}
+                 source={require('../../assets/profilePic.jpg')}
+            />
+            <Text style={tw`ml-5`} >For Someone</Text>
+            <Icon
+               type='material-community'
+               name='chevron-down'
+               color="#0c0c0c"
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+      </View>
+      
       <MapComponent/>
-    </View>
+      </View>
+    
   )
 }
 
